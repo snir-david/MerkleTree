91899915195a905e53d77bf0d31bbf72e3cfefaf
Snir David Nahari <snir-david@users.noreply.github.com>
2021-05-23 13:18:50 +0300

    re-coding everything until checking proof

--- MerkelTree.py
+++ MerkelTree.py
@@ -2,139 +2,145 @@ import hashlib
 
 
 class Node:
-
     def __init__(self, data):
+        # nodes
         self.left = None
         self.right = None
         self.father = None
+        # node level
+        self.level = 1
+        # data and hash
         self.data = data
         digest = hashlib.sha256()
         digest.update(str(data).encode())
-        self.digest = digest
-
-    def add(self, data):
-        # Compare the new value with the parent node
-        curr_digest = hashlib.sha256()
-        curr_digest.update(str(data).encode())
-        if self.digest:
-            if curr_digest.hexdigest() < self.digest.hexdigest():
-                if self.left is None:
-                    self.left = Node(data)
-                    self.right = Node(self.data)
-                    self.left.father = self.right.father = self
-                    self.recalc_tree()
-                else:
-                    self.left.add(data)
-            elif curr_digest.hexdigest() > self.digest.hexdigest():
-                if self.right is None:
-                    self.right = Node(data)
-                    self.left = Node(self.data)
-                    self.left.father = self.right.father = self
-                    self.recalc_tree()
-                else:
-                    self.right.add(data)
-        else:
-            self.digest = curr_digest
-
-    # Print the merkel tree
-    def print_tree(self):
-        if self.left:
-            self.left.print_tree()
-        print(self.data, self.digest.hexdigest(), self, self.father)
-        if self.right:
-            self.right.print_tree()
+        self.hash = digest.hexdigest()
 
     def recalc_tree(self):
         if self.father is None:
             digest = hashlib.sha256()
-            digest.update(bytes(self.left.digest.hexdigest(), 'utf8'))
-            digest.update(bytes(self.right.digest.hexdigest(), 'utf8'))
-            self.digest = digest
+            digest.update(bytes(str(self.left.hash) + str(self.right.hash), 'utf8'))
+            self.hash = digest.hexdigest()
             self.data = str(self.left.data) + str(self.right.data)
         else:
             digest = hashlib.sha256()
-            digest.update(bytes(self.left.digest.hexdigest(), 'utf8'))
-            digest.update(bytes(self.right.digest.hexdigest(), 'utf8'))
-            self.digest = digest
+            digest.update(bytes(str(self.left.hash) + str(self.right.hash), 'utf8'))
+            self.hash = digest.hexdigest()
             self.data = str(self.left.data) + str(self.right.data)
             self.father.recalc_tree()
 
-    def get_root(self):
-        if self.father is None:
-            return self.digest.hexdigest()
-        self.father.get_root()
+    # Print the merkle tree
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data, self.hash)
+        if self.right:
+            self.right.print_tree()
 
-    def find_leaf(self, leaf_num):
-        leaf_list = []
-        nodes_visited = []
-        while len(leaf_list) != leaf_num + 1:
-            if self.left is None and self.right is None:
-                leaf_list.append(self)
-                nodes_visited.append(self)
-                self = self.father
-            else:
-                if self.left in nodes_visited:
-                    if self.right in nodes_visited:
-                        nodes_visited.append(self)
-                        self = self.father.right
-                    else:
-                        self = self.right
-                else:
-                    self = self.left
-        return leaf_list.pop()
+
+class MerkleTree:
+
+    def __init__(self, data):
+        # node
+        self.tree_root = Node(data)
+        # tech details
+        self.tree_size = 1
+        self.leaves = [self.tree_root]
+
+    def add(self, data):
+        inc_tree_size = True
+        # finding where need to add the new leaf
+        for leaf in self.leaves:
+            if leaf.level != self.tree_size:
+                inc_tree_size = False
+                break
+        # if need to add new level return to root and adding new leaf
+        while leaf.father is not None and inc_tree_size:
+            leaf = leaf.father
+        # adding new leaf to the right and recalculating the tree
+        r_node = Node(data)
+        f_node = Node('tmp')
+        f_node.left = leaf
+        f_node.right = r_node
+        leaf.father = r_node.father = f_node
+        f_node.recalc_tree()
+        # adding leaf to leaves list
+        self.leaves.append(r_node)
+        # if needed increase tree size and leaves level and change root to current father node
+        if inc_tree_size:
+            self.tree_root = f_node
+            self.tree_size += 1
+            for leaf in self.leaves:
+                leaf.level += 1
+        else:
+            self.tree_root.right = f_node
+            f_node.father = self.tree_root
+            self.tree_root.recalc_tree()
+
+    def get_root(self):
+        return self.tree_root.hash
 
     def get_proof(self, leaf_num):
-        leaf = self.find_leaf(leaf_num)
+        leaf = self.leaves[leaf_num]
         father = leaf.father
         proof = ""
-        if leaf != father.right:
-            proof += str(father.right.data)
-        else:
-            proof += str(father.left.data)
-        leaf = father
-        father = father.father
         while father is not None:
             if leaf != father.right:
-                proof += " " + str(father.right.data)
+                proof += str(father.right.hash) + " "
             else:
-                proof += " " + str(father.left.data)
+                proof += str(father.left.hash) + " "
             leaf = father
             father = father.father
         root = str(self.get_root())
-        root += " " + proof
+        root += " " + proof[:len(proof) - 1]
         return root
 
     def check_proof(self, inc_proof):
+        # splitting string for proof
         hash_list = inc_proof.split(" ")
+        # creating new list without root
         hash_without_root = []
         for i in range(0, len(hash_list)):
             if i != 1:
                 hash_without_root.append(hash_list[i])
-        digest = hashlib.sha256()
-        tmp_digest = hashlib.sha256()
-        # digest.update(bytes(hash_without_root[0] + hash_without_root[1], 'utf8'))
-        # print(digest.hexdigest())
-        for i in range(0, len(hash_without_root)-1):
-            digest.update(bytes(hash_without_root[i] + hash_without_root[i+1], 'utf8'))
-            hash_without_root[i+1] = digest.hexdigest()
-            print(digest.hexdigest())
-        print(digest.hexdigest())
+        # checking for every proof if it is right or left node in the tree
+        node = self.tree_root
+        node_location = []
+        for i in range(0, len(hash_without_root) - 1):
+            if node.left.hash == hash_without_root[len(hash_without_root) - (i + 1)]:
+                node_location.append("left")
+                node = node.right
+            elif node.right.hash == hash_without_root[len(hash_without_root) - (i + 1)]:
+                node_location.append("right")
+                node = node.left
+            else:
+                print("hash not in tree")
+        # reverse locations list and start iterating from leaves to root and calculating hash function
+        node_location.reverse()
+        for i in range(0, len(hash_without_root) - 1):
+            digest = hashlib.sha256()
+            # if proof is from the right
+            if node_location[i] is 'right':
+                digest.update((hash_without_root[i] + hash_without_root[i + 1]).encode())
+
+            # if proof is from the left
+            if node_location[i] is 'left':
+                digest.update((hash_without_root[i + 1] + hash_without_root[i]).encode())
+            hash_without_root[i + 1] = digest.hexdigest()
         if digest.hexdigest() == hash_list[1]:
             return True
         return False
 
 
 # Use the insert method to add nodes
-root = Node(1)
+root = MerkleTree(1)
 root.add(2)
 root.add(3)
 root.add(4)
 print("print tree")
-root.print_tree()
+root.tree_root.print_tree()
 print("print tree root")
 print(root.get_root())
-node = root.find_leaf(2)
 proof = root.get_proof(2)
 print("print prof")
 print(proof)
-root.check_proof(str(node.digest.hexdigest()) + " " + proof)
+root.check_proof(root.leaves[2].hash + " " + proof)
